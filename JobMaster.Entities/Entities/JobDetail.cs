f9973d967797e58d90e4494609f52cd821f526e8
Aswathy VS <aswathyachus.86@gmail.com>
2018-06-01 13:54:03 +0530

    Entity changes

--- JobMaster.Entities/Entities/JobDetail.cs
+++ JobMaster.Entities/Entities/JobDetail.cs
@@ -6,7 +6,7 @@ namespace JobMaster
 {
     public class JobDetail : BaseEntity
     {
-        public string JobName { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public int Salary { get; set; } 
         public int ParentId { get; set; }
