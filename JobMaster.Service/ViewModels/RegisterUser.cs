18d69ca049a8117c3b65741564415d41ef31e606
nayassubramanian <nayassubramanian899@gmail.com>
2018-06-02 22:01:15 +0530

    Added register screen

---
+++ JobMaster.Service/ViewModels/RegisterUser.cs
@@ -0,0 +1,35 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace JobMaster.Service.ViewModels
+{
+    public class RegisterUser
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        [StringLength(50, MinimumLength = 4)]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [StringLength(50)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 5)]
+        [CompareAttribute("ConfirmPassword", ErrorMessage = "The Password and Confirm Password fields did not match.")]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [StringLength(50, MinimumLength = 5)]
+        [CompareAttribute("Password", ErrorMessage = "The Password and Confirm Password fields did not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ JobMaster.Web/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿
+using JobMaster.Service.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobMaster.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([Bind] RegisterUser register)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+                return View();
+        }
+
+    }
+}
